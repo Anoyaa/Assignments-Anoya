29555d995648e3b6ec725a0330d45e10a2691795
Anoyaa <anoya.a@geekywolf.com>
2024-09-25 18:29:32 +0530

    Add files via upload

---
+++ repos/assignment2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ class Programa
+ {
+    public static void Main (string[] args)
+    {
+        int i = 1;
+        while (i<50)
+        {
+            if (i%2!=0)
+            {
+                Console.WriteLine(i);
+            }
+            i++;
+        }
+        Console.ReadLine();
+    }
+ }
+
---
+++ repos/assignment5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int number, digit=0, sum = 0;
+        Console.WriteLine("enter the number whose sum of digits is to be found");
+        number=Convert.ToInt32(Console.ReadLine());
+        while(number>0)
+        {
+            digit = number % 10;
+            sum= sum+digit;
+            number = number / 10;
+        }
+        Console.WriteLine("the sume of digits of a number is"+sum);
+        Console.ReadLine();
+    }
+}
