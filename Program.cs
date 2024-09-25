e39458fbcafc1e83cab8c96e9d0f2b092d1b03a0
Anoyaa <anoya.a@geekywolf.com>
2024-09-25 18:24:34 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,37 @@
 ﻿using System;
-
-    class Sumofnum
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class Program3
+{
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        int number1, number2, number3;
+        Console.WriteLine("enter the 3 numbers");
+        number1=Convert.ToInt16(Console.ReadLine());
+        number2 =Convert.ToInt16(Console.ReadLine());
+        number3 =Convert.ToInt16(Console.ReadLine());
+        if (number1> number2 && number1 > number3)
         {
-            int a,c;
-            int b;
-            Console.WriteLine("enter the 1st and 2nd number");
-            a= Convert.ToInt32(Console.ReadLine());
-            b= Convert.ToInt32(Console.ReadLine());
-            c = a + b;
-            Console.WriteLine("the sum is" + c) ;
-        //Console.ReadLine();
-
+            Console.WriteLine("greatest is" + number1);
         }
+        else if (number2 >number1 && number2 > number3)
+        { Console.WriteLine("greatest is " + number2);
+        }
+        else
+        {
+            Console.WriteLine("greatest is" + number3);
+        }
+        Console.ReadLine();
     }
+}
+            //  {
+            //       Console.WriteLine("greatest is" + a) ;
+            //  }
+            //   else if(b>c)
+            //  {
+            //       Console.WriteLine("greatest is"+b);
+            //   }
+            //  }
+            //  else if(a<b)
+     
