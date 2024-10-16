8b62d6a87e108d099174319f5041ebb99cad2b2a
A Anoya <anoya.a@geekywolf.local>
2024-10-16 14:47:01 +0530

    angular dropdown filter_removed null spaces in dropdown list

---
+++ emp-dropdown/src/src/app/iemployee-form.ts
@@ -0,0 +1,16 @@
+import { FormControl, FormGroup } from "@angular/forms";
+
+export interface IemployeeForm {
+    firstName:FormControl<string>;
+    lastName:FormControl<string>;
+    email:FormControl<string>;
+    phone:FormControl<string>;
+    address:FormGroup<
+    {
+      building:FormControl<string>;
+      streetName:FormControl<string>;
+      city:FormControl<string>;
+    }>;
+    project:FormControl<string>
+
+}
