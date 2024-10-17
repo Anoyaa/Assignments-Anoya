2f116fe087980ebea95390702ecb3fe3ee86d1a0
A Anoya <anoya.a@geekywolf.local>
2024-10-17 17:22:14 +0530

    angular hovername

---
+++ angular_hover/src/app/emp-hover.directive.spec.ts
@@ -0,0 +1,8 @@
+import { EmpHoverDirective } from './emp-hover.directive';
+
+describe('EmpHoverDirective', () => {
+  it('should create an instance', () => {
+    const directive = new EmpHoverDirective();
+    expect(directive).toBeTruthy();
+  });
+});
